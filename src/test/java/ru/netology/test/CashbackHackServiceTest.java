8d882bd2e5adab1c567dd82dc9d9e50cd5909634
Ольга Евстифеева <olga1989nikol@rambler.ru>
2022-09-05 10:39:54 +0300

    JUnit4 added

---
+++ src/test/java/ru/netology/test/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.test;
+
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldBuyLessThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuyMoreFor1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+}
